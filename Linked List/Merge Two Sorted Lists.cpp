fc80004846ed92867477c8f65fa71601de61cb37
PinguNootNoot <MP5K1234@gmail.com>
2026-02-09 17:45:55 +0800

    Minor update Feb(2026)

--- Linked List/Merge Two Sorted Lists.cpp
+++ Linked List/Merge Two Sorted Lists.cpp
@@ -4,45 +4,30 @@ using namespace std;
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode *curNode1, *curNode2;
+        if(list1 == nullptr) return list2;
+        if(list2 == nullptr) return list1;
 
-        if(list1 && !list2) return list1;
-        if(!list1 && list2) return list2;
-        if(!list1 && !list2) return nullptr;
-
-        curNode1 = list1; curNode2 = list2;
-        while(curNode1 || curNode2) {
-            if(curNode1) {
-                if(M.find(curNode1->val) != M.end()) {
-                    ++M.find(curNode1->val)->second;
-                } else {
-                    M.insert(pair<int,int>(curNode1->val,1));
+        ListNode *l1 = list1, *l2 = list2;
+        ListNode *head, *tail;
+        while(l1 != nullptr && l2 != nullptr) {
+            if(l1->val <= l2->val) {
+                head = l1;
+                while(l1) {
+                    if(l1->val > l2->val) break;
+                    tail = l1;
+                    l1 = l1->next;
                 }
-                curNode1 = curNode1->next;
-            } if(curNode2) {
-                if(M.find(curNode2->val) != M.end()) {
-                    ++M.find(curNode2->val)->second;
-                } else {
-                    M.insert(pair<int,int>(curNode2->val,1));
+                tail->next = l2;
+            } else {
+                head = l2;
+                while(l2) {
+                    if(l1->val <= l2->val) break;
+                    tail = l2;
+                    l2 = l2->next;
                 }
-                curNode2 = curNode2->next;
+                tail->next = l1;
             }
         }
-
-        ListNode *newHead, *temp;
-
-        newHead = new ListNode(M.begin()->first); temp = newHead;
-        --M.begin()->second;
-        for(auto i = M.begin(); i != M.end(); ++i) {
-            while(i->second > 0) {
-                temp->next = new ListNode(i->first);
-                --i->second;
-                temp = temp->next;
-            }
-        }
-        return newHead;
+        return (list1->val <= list2->val) ? list1 : list2;
     }
-
-private:
-    map<int,int> M;
 };
